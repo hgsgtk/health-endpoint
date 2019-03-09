7fd8a838cc2bf9dfd690d33bcd14cdc08a84254e
Kazuki Higashiguchi <k.higashiguchi@binc.jp>
2019-03-09 10:20:36 +0900

    add go.mod and go.sum

--- src/api/simple-health-handler.go
+++ src/api/simple-health-handler.go
@@ -2,20 +2,23 @@ package api
 
 import "net/http"
 
-// SimpleHealthCheck return 200.
-func SimpleHealthCheck(w http.ResponseWriter, r *http.Request) {
-	w.WriteHeader(http.StatusOK)
-}
-
 type SimpleResponse struct {
 	Status  int    `json:"status"`
-	Message string `json:"message"`
+	Message string `json:"message,omitempty"`
 	Detail  string `json:"string,omitempty"`
+}
+
+func SimpleHealthCheck(w http.ResponseWriter, r *http.Request) {
+	rs := SimpleResponse{
+		Status: http.StatusOK,
+	}
+	respondJson(w, rs, http.StatusOK)
 }
 
 func SimpleDeepHealthCheck(w http.ResponseWriter, r *http.Request) {
 	_, err := NewMySQL(config.DB)
 	if err != nil {
+		// DBへのコネクションにてエラーが発生した場合は503レスポンス
 		rs := SimpleResponse{
 			Status:  http.StatusServiceUnavailable,
 			Message: "failed to get connection database",
